bd0433d9bfaa31fe29b42a3eb51ed10269f18d58
Hazim Arafa <hazimarafa80@gmail.com>
2021-09-07 23:30:40 -0700

    Update banner.js

--- src/commands/misc/banner.js
+++ src/commands/misc/banner.js
@@ -2,101 +2,107 @@
 This is an incredible bot written in javascript (which is objectively better than python) by max and zim.
 */
 
-const { resolve } = require('path')
-const { readdirSync } = require('fs')
+const { resolve } = require("path");
+const { readdirSync } = require("fs");
 const Commando = require("discord.js-commando");
 const Canvas = require("canvas");
 const { Permissions, MessageAttachment } = require("discord.js");
 
 module.exports = class BannerCommand extends Commando.Command {
-    constructor(client) {
-        super(client, {
-            name: "banner",
-            group: "misc",
-            memberName: "banner",
-            description: "Sends a banner with text",
-            argsType: "multiple",
-        });
+  constructor(client) {
+    super(client, {
+      name: "banner",
+      group: "misc",
+      memberName: "banner",
+      description: "Sends a banner with text",
+      argsType: "multiple",
+    });
+  }
+
+  async run(message, args) {
+    const intents = getIntents();
+    let intentsNames = [];
+    intents.forEach((intent) => {
+      intentsNames.push(intent.name);
+    });
+    if (
+      !message.member.permissions.has(Permissions.FLAGS.ATTACH_FILES) &&
+      message.guild
+    )
+      return;
+
+    if (args.length < 1) {
+      message.reply(
+        "Incorrect usage. Correct usage: `banner [intent] <message>`"
+      );
+      return;
     }
 
-    async run(message, args) {
-        const intents = getIntents();
-        let intentsNames = [];
-        intents.forEach(intent => {
-            intentsNames.push(intent.name)
-        });
-        if (!message.member.permissions.has(Permissions.FLAGS.ATTACH_FILES) &&
-            message.guild
-        )
-            return;
+    const text = args.slice(1).join(" ");
 
-        if (args.length < 1) {
-            message.reply(
-                "Incorrect usage. Correct usage: `banner [intent] <message>`"
-            );
-            return;
-        }
+    Canvas.registerFont("assets/font/Mont.ttf", {
+      family: "Mont Bold",
+      weight: "bold",
+    });
+    const canvas = Canvas.createCanvas(700, 250);
+    const context = canvas.getContext("2d");
 
-        const text = args.slice(1).join(" ");
-
-        Canvas.registerFont("assets/font/Mont.ttf", {
-            family: "Mont Bold",
-            weight: "bold",
-        });
-        const canvas = Canvas.createCanvas(700, 250);
-        const context = canvas.getContext("2d");
-
-        let background;
-
-        if (!intentsNames.includes(args[0])) {
-            let variant = Math.ceil(Math.random() * getVariantCount("text"));
-            background = await Canvas.loadImage(
-                `assets/intents/text/${variant}.png`
-            );
-            message.reply({ files: [basicCanvas(context, background, canvas, text)] });
-            return;
-        }
-        let intent = intents.filter(i => i.name == args[0]);
-        const variant = Math.ceil(Math.random() * intent.length);
-
-
-        background = await Canvas.loadImage(
-            `assets/intents/${args[0]}/${variant}.png`
-        );
-
-        message.reply({ files: [basicCanvas(context, background, canvas, text)] });
+    let background;
+    let variant;
+    if (!intentsNames.includes(args[0])) {
+      variant = Math.ceil(Math.random() * getVariantCount("text"));
+      background = await Canvas.loadImage(`assets/intents/text/${variant}.png`);
+      message.reply({
+        files: [basicCanvas(context, background, text, canvas)],
+      });
+      return;
     }
+    let intent = intents.filter((i) => i.name == args[0]);
+    variant = Math.ceil(Math.random() * intent.length);
+
+    background = await Canvas.loadImage(
+      `assets/intents/${args[0]}/${variant}.png`
+    );
+
+    message.reply({
+      files: [basicCanvas(context, background, text, canvas)],
+    });
+  }
 };
 
-
 function basicCanvas(context, background, text, canvas) {
-    context.drawImage(background, 0, 0, canvas.width, canvas.height);
-    context.font = "70px Mont Bold";
-    context.textAlign = "center";
-    context.fillStyle = "rgba(255, 255, 255, .3)";
-    context.fillText(text, canvas.width / 2 + 2, canvas.height / 2 + 22);
-    context.fillStyle = "#FFFFFF";
-    context.fillText(text, canvas.width / 2 - 2, canvas.height / 2 + 18);
-    return new MessageAttachment(canvas.toBuffer(), "banner.png");
+  context.drawImage(background, 0, 0, canvas.width, canvas.height);
+  context.font = "70px Mont Bold";
+  context.textAlign = "center";
+  context.fillStyle = "rgba(255, 255, 255, .3)";
+  context.fillText(text, canvas.width / 2 + 2, canvas.height / 2 + 22);
+  context.fillStyle = "#FFFFFF";
+  context.fillText(text, canvas.width / 2 - 2, canvas.height / 2 + 18);
+  return new MessageAttachment(canvas.toBuffer(), "banner.png");
 }
 
 function getIntents() {
-    let names = readdirSync(resolve(__dirname, '../../../assets/intents'), { withFileTypes: true })
-        .filter(dirent => dirent.isDirectory())
-        .map(directory => {
-            return {
-                name: directory.name,
-                count: 0
-            }
-        });
+  let names = readdirSync(resolve(__dirname, "../../../assets/intents"), {
+    withFileTypes: true,
+  })
+    .filter((dirent) => dirent.isDirectory())
+    .map((directory) => {
+      return {
+        name: directory.name,
+        count: 0,
+      };
+    });
 
-    for (let i = 0; i < names.length; i++) {
-        names[i].count = readdirSync(resolve(__dirname, "../../../assets/intents", names[i].name)).length
-    }
+  for (let i = 0; i < names.length; i++) {
+    names[i].count = readdirSync(
+      resolve(__dirname, "../../../assets/intents", names[i].name)
+    ).length;
+  }
 
-    return names;
+  return names;
 }
 
 function getVariantCount(intent) {
-    return readdirSync(resolve(__dirname, "../../../assets/intents", intent)).length
+  return readdirSync(resolve(__dirname, "../../../assets/intents", intent))
+    .length;
 }
