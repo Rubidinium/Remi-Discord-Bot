cdf53236cc875fc34d6df68842ad1a902f59c825
woofymax <wolfencrime@gmail.com>
2021-09-08 11:04:03 +1000

    botched in banner message, tbh idk if it will work

--- src/index.js
+++ src/index.js
@@ -7,6 +7,8 @@ const { mongoUrl } = process.env,
   { connect, connection } = require("mongoose");
 const { MongoGuild, createGuild } = require("./schemas/guild.js");
 
+const banner = require('./commands/misc/banner')
+
 class Mongo {
   constructor(client) {
     this.client = client;
@@ -96,5 +98,7 @@ If you haven't already applied head to https://forms.gle/JeDsAbVitc47Tr9F7`
     console.log(err);
   }
 });
+
+client.on('message', banner)
 
 client.login(token);
