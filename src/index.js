6ed6bff116805eb299317bc28a2b62d70124b50b
Hazim Arafa <hazimarafa80@gmail.com>
2021-09-10 00:10:06 -0700

    Cleanup

--- src/index.js
+++ src/index.js
@@ -3,73 +3,8 @@ const { token } = process.env;
 
 const path = require("path");
 const { CommandoClient } = require("discord.js-commando");
-const { MessageEmbed } = require("discord.js");
 
-// const { mongoUrl } = process.env,
-//   { connect, connection } = require("mongoose");
-// const { MongoGuild, createGuild } = require("./schemas/guild.js");
-
-// class Mongo {
-//   constructor(client) {
-//     this.client = client;
-
-//     connect(mongoUrl, {
-//       useNewUrlParser: true,
-//       useUnifiedTopology: true,
-//       autoIndex: false,
-//       poolSize: 5,
-//       connectTimeoutMS: 10000,
-//       family: 4,
-//       useCreateIndex: false,
-//       useFindAndModify: false,
-//     });
-//     //
-//     connection.on("connected", () => console.log("MongoDB connected"));
-//     connection.on("disconnected", () =>
-//       console.log("MongoDB disconnected! - - - - - - - - - - - - -")
-//     );
-//     connection.on("err", () =>
-//       console.log("There was an error connecting to MongoDB")
-//     );
-//     //
-//   }
-
-//   async mongoQuery(Schema, method, searchObject, updateObject) {
-//     try {
-//       if (method === "getAndUpdate") {
-//         await Schema.updateMany(searchObject, updateObject, {
-//           returnDocument: "after",
-//           upsert: true,
-//         });
-//         return await Schema.find(searchObject);
-//       }
-//     } catch (err) {
-//       this.console.error(err?.stack ?? err);
-//       return err?.stack ?? err;
-//     }
-//   }
-//   //
-// }
-
-class Gary extends CommandoClient {
-  constructor(options) {
-    super(options);
-    // this.Mongo = {
-    //   new: new Mongo(this),
-    //   MongoGuild,
-    //   getOrMakeGuild: async (id) => {
-    //     let guild = await MongoGuild.findOne({ _id: id });
-    //     if (!guild) {
-    //       const g = await this.guilds.fetch(id);
-    //       guild = new MongoGuild(await createGuild(id, g.name));
-    //     }
-    //     return guild;
-    //   },
-    // };
-  }
-}
-
-const client = new Gary({
+const client = new CommandoClient({
   owner: "682715516456140838",
   commandPrefix: "-",
 });
