182d582077ad3b889e0730c5eb2b83f87b435092
Hazim Arafa <hazimarafa80@gmail.com>
2021-09-10 11:17:23 -0700

    index

--- src/index.js
+++ src/index.js
@@ -9,19 +9,19 @@ const client = new CommandoClient({
   commandPrefix: "-",
 });
 
-client.once("ready", async() => {
-    console.log("Gary is ready :)");
-    console.log(path.join(__dirname, "commands"));
-    client.user.setActivity(`${client.commandPrefix}help`, {
-        type: "LISTENING",
-    });
-    client.registry
-        .registerGroups([
-            ["misc", "Misc commands"],
-            // ["moderation", "Moderation commands"],
-        ])
-        .registerDefaults()
-        .registerCommandsIn(path.join(__dirname, "commands"));
+client.once("ready", async () => {
+  console.log("Gary is ready :)");
+  console.log(path.join(__dirname, "commands"));
+  client.user.setActivity(`${client.commandPrefix}help`, {
+    type: "LISTENING",
+  });
+  client.registry
+    .registerGroups([
+      ["misc", "Misc commands"],
+      // ["moderation", "Moderation commands"],
+    ])
+    .registerDefaults()
+    .registerCommandsIn(path.join(__dirname, "commands"));
 });
 
 client.login(token);
