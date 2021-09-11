967656cce9d2691661e908876dddf7fac3188e1a
mx-mw <wolfencrime@gmail.com>
2021-09-11 18:52:57 +1000

    added check for messages that are not suggestions in suggestions channel

--- src/index.js
+++ src/index.js
@@ -5,22 +5,28 @@ const { join } = require("path");
 const { CommandoClient } = require("discord.js-commando");
 
 const client = new CommandoClient({
-  owner: "682715516456140838",
-  commandPrefix: "-",
+    owner: "682715516456140838",
+    commandPrefix: "-",
 });
 
-client.once("ready", async () => {
-  console.log("Gary is ready :)");
-  console.log(join(__dirname, "commands"));
-  
-  client.user.setActivity(`${client.commandPrefix}help`, {
-    type: "LISTENING",
-  });
+client.once("ready", async() => {
+    console.log("Gary is ready :)");
+    console.log(join(__dirname, "commands"));
 
-  client.registry
-    .registerGroups([["misc", "Misc commands"]])
-    .registerDefaults()
-    .registerCommandsIn(join(__dirname, "commands"));
+    client.user.setActivity(`${client.commandPrefix}help`, {
+        type: "LISTENING",
+    });
+
+    client.registry
+        .registerGroups([
+            ["misc", "Misc commands"]
+        ])
+        .registerDefaults()
+        .registerCommandsIn(join(__dirname, "commands"));
 });
+
+client.on('message', message => {
+    if (message.channel.id == '885259801309876254' && !message.content.startsWith('-suggest')) message.delete();
+})
 
 client.login(token);
