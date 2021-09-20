dec93bb5f4c8640d060c72f7f6af8a119c8fb8a7
Hazim Arafa <hazimarafa80@gmail.com>
2021-09-19 18:48:40 -0700

    courses

--- src/commands/courses.js
+++ src/commands/courses.js
@@ -2,21 +2,61 @@ import { SlashCommandBuilder } from "@discordjs/builders";
 import PaginationEmbed from "../embeds/PaginationEmbed.js";
 
 const command = new SlashCommandBuilder()
-    .setName("courses")
-    .setDescription("Info about the courses");
+  .setName("courses")
+  .setDescription("Info about the courses");
+
+const courses = [
+  {
+    name: "Web Development",
+    id: "web",
+    description: "Learn how to build a website",
+    price: "FREE",
+    duration: "4 weeks",
+    image: "https://i.imgur.com/qQqxQ8l.png",
+  },
+  {
+    name: "Game Development",
+    id: "game",
+    description: "Learn how to build a game",
+    price: "FREE",
+    duration: "4 weeks",
+    image: "https://i.imgur.com/qQqxQ8l.png",
+  },
+  {
+    name: "Discord Bot Development (JS)",
+    id: "bot",
+    description: "Learn how to build a discord bot",
+    price: "FREE",
+    duration: "4 weeks",
+    image: "https://i.imgur.com/qQqxQ8l.png",
+  },
+  {
+    name: "Artificial Intelligence",
+    id: "ai",
+    description: "Learn how to build a AI",
+    price: "FREE",
+    duration: "8 weeks",
+    image: "https://i.imgur.com/qQqxQ8l.png",
+  },
+];
 
 export default {
-    data: command.toJSON(),
-    async execute(interaction, parent) {
-        interaction.reply(new PaginationEmbed({
-            title: 'test',
-            description: 'ksdjflksdf',
-            fields: [
-                { name: 'Test field', value: 'hahahhewhawhe' }
-            ]
-        }, 5))
-    },
+  data: command.toJSON(),
+  async execute(interaction, parent) {
+    interaction.reply(
+      new PaginationEmbed(
+        {
+          title: "test",
+          description: "ksdjflksdf",
+          fields: [{ name: "Test field", value: "hahahhewhawhe" }],
+        },
+        5
+      )
+    );
+  },
 };
+
+export { courses };
 
 /*
 this.embed = new MessageEmbed()
