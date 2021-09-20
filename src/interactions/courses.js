284ccfb2b75811c2ffbe356dce5f754e84698e4d
mx-mw <wolfencrime@gmail.com>
2021-09-20 13:26:26 +1000

    fixed api error

--- src/interactions/courses.js
+++ src/interactions/courses.js
@@ -5,40 +5,40 @@ import PaginationEmbed from "../embeds/PaginationEmbed.js";
 const buttons = new Collection();
 
 buttons.set("back", (interaction) => {
-  const currentPage = 1;
-  interaction.message.edit(
-    new PaginationEmbed({
-      title: courses[currentPage - 1].name,
-      description: courses[currentPage - 1].description,
-      fields: [
-        { name: "Duration", value: courses[currentPage - 1].duration },
-        { name: "Price", value: courses[currentPage - 1].price, inline: true },
-      ],
-    })
-  );
+	const currentPage = interaction.message.embeds[0].footer.text.slice(5).split('/')[0];
+
+	interaction.update(
+		new PaginationEmbed({
+			title: courses[currentPage - 1].name,
+			description: courses[currentPage - 1].description,
+			fields: [
+				{ name: "Duration", value: courses[currentPage - 1].duration },
+				{ name: "Price", value: courses[currentPage - 1].price, inline: true },
+			],
+		})
+	);
 });
 
 buttons.set("forward", (interaction) => {
-  console.log(interaction);
-  const currentPage = 1;
-  interaction.message.edit(
-    new PaginationEmbed({
-      title: courses[currentPage].name,
-      description: courses[currentPage].description,
-      fields: [
-        {
-          name: "Duration",
-          value: courses[currentPage].duration,
-          inline: true,
-        },
-        { name: "Price", value: courses[currentPage].price, inline: true },
-      ],
-    })
-  );
+	const currentPage = interaction.message.embeds[0].footer.text.slice(5).split('/')[0];
+	interaction.update(
+		new PaginationEmbed({
+			title: courses[currentPage].name,
+			description: courses[currentPage].description,
+			fields: [
+				{
+					name: "Duration",
+					value: courses[currentPage].duration,
+					inline: true,
+				},
+				{ name: "Price", value: courses[currentPage].price, inline: true },
+			],
+		})
+	);
 });
 
 export const handle = (interaction, client) => {
-  buttons.get(interaction.customId)();
+	buttons.get(interaction.customId)(interaction);
 };
 
 export { buttons };
