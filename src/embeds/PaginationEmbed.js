212fc7ef18b00a6308e03d288468fc55a2f13141
Hazim Arafa <hazimarafa80@gmail.com>
2021-09-19 19:41:45 -0700

    next/back

--- src/embeds/PaginationEmbed.js
+++ src/embeds/PaginationEmbed.js
@@ -1,29 +1,29 @@
 import { MessageEmbed, MessageActionRow, MessageButton } from "discord.js";
+import { courses } from "../commands/courses.js";
 
 export default class PaginationEmbed {
-    constructor(page, pageCount, client) {
-        this.embed = new MessageEmbed()
-            .setColor('RANDOM')
-            .setTitle(page.title)
-            .setAuthor('Gary')
-            .setDescription(page.description)
-            .addFields(page.fields)
-            .setFooter(`Page 1/${pageCount}`)
+  constructor(page, client) {
+    this.embed = new MessageEmbed()
+      .setColor("RANDOM")
+      .setTitle(page.title)
+      .setAuthor("Gary")
+      .setDescription(page.description)
+      .addFields(page.fields)
+      .setFooter(`Page 1/${courses.length}`);
 
-        this.row = new MessageActionRow()
-            .addComponents(
-                new MessageButton()
-                    .setCustomId('back')
-                    .setEmoji('889310059899797534')
-                    .setStyle('PRIMARY'),
-                new MessageButton()
-                    .setCustomId('forward')
-                    .setEmoji('889310059903975495')
-                    .setStyle('PRIMARY')
-            )
-        return {
-            embeds: [this.embed],
-            components: [this.row]
-        }
-    }
+    this.row = new MessageActionRow().addComponents(
+      new MessageButton()
+        .setCustomId("back")
+        .setEmoji("889310059899797534")
+        .setStyle("PRIMARY"),
+      new MessageButton()
+        .setCustomId("forward")
+        .setEmoji("889310059903975495")
+        .setStyle("PRIMARY")
+    );
+    return {
+      embeds: [this.embed],
+      components: [this.row],
+    };
+  }
 }
