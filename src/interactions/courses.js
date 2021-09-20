913781c37d89647028705cd68ee322dc6598b398
Hazim Arafa <hazimarafa80@gmail.com>
2021-09-19 20:44:52 -0700

    Update courses.js

--- src/interactions/courses.js
+++ src/interactions/courses.js
@@ -5,9 +5,9 @@ import PaginationEmbed from "../embeds/PaginationEmbed.js";
 const buttons = new Collection();
 
 buttons.set("back", (interaction) => {
-  const currentPage = parseInt(
-    interaction.message.embeds[0].footer.text.slice(5).split("/")[0]
-  );
+  const currentPage =
+    parseInt(interaction.message.embeds[0].footer.text.slice(5).split("/")[0]) -
+    1;
 
   interaction.update(
     new PaginationEmbed(
@@ -15,14 +15,18 @@ buttons.set("back", (interaction) => {
         title: courses[currentPage - 1].name,
         description: courses[currentPage - 1].description,
         fields: [
-          { name: "Duration", value: courses[currentPage - 1].duration },
+          {
+            name: "Duration",
+            value: courses[currentPage - 1].duration,
+            inline: true,
+          },
           {
             name: "Price",
             value: courses[currentPage - 1].price,
             inline: true,
           },
         ],
-        course: courses[currentPage],
+        course: courses[currentPage - 1],
       },
       currentPage
     )
@@ -30,23 +34,27 @@ buttons.set("back", (interaction) => {
 });
 
 buttons.set("forward", (interaction) => {
-  const currentPage = interaction.message.embeds[0].footer.text
-    .slice(5)
-    .split("/")[0];
+  const currentPage =
+    parseInt(interaction.message.embeds[0].footer.text.slice(5).split("/")[0]) -
+    1;
   interaction.update(
     new PaginationEmbed(
       {
-        title: courses[currentPage].name,
-        description: courses[currentPage].description,
+        title: courses[currentPage + 1].name,
+        description: courses[currentPage + 1].description,
         fields: [
           {
             name: "Duration",
-            value: courses[currentPage].duration,
+            value: courses[currentPage + 1].duration,
             inline: true,
           },
-          { name: "Price", value: courses[currentPage].price, inline: true },
+          {
+            name: "Price",
+            value: courses[currentPage + 1].price,
+            inline: true,
+          },
         ],
-        course: courses[currentPage],
+        course: courses[currentPage + 1],
       },
       currentPage
     )
