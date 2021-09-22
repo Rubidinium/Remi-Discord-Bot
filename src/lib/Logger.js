287c35f5b7b6eafedcf7fa25a3ebb4479f095e36
mx-mw <wolfencrime@gmail.com>
2021-09-22 13:21:24 +1000

    i did something i guess

--- src/lib/Logger.js
+++ src/lib/Logger.js
@@ -1,6 +1,6 @@
 import { Socket } from 'net';
 import { appendFileSync } from 'fs';
-import { GuildChannel, Collection } from 'discord.js';
+import { GuildChannel, Collection, MessageEmbed } from 'discord.js';
 import moment from 'moment';
 
 export default class Logger {
@@ -31,17 +31,41 @@ export default class Logger {
         this.exitOnError = config.exitOnError;
         this.showLoggingLevel = config.showLoggerLevel;
 
+        this.levels = new Collection();
+
+        this.levels.set(4, {
+            color: 'GREEN',
+            method: this.sill,
+            name: 'sill',
+        })
+        this.levels.set(3, {
+            color: 'BLUE',
+            method: this.info,
+            name: 'info',
+        })
+        this.levels.set(2, {
+            color: 'YELLOW',
+            method: this.warn,
+            name: 'warn',
+        })
+        this.levels.set(1, {
+            color: 'RED',
+            method: this.error,
+            name: 'error',
+        })
+
+
         this.handlers = new Collection();
         config.targets.forEach(i => {
             switch (i.type) {
                 case 'discordChannel':
-                    this.handlers.set('discordChannel', { execute: discordChannel, value: i.value });
+                    this.handlers.set('discordChannel', { execute: this.discordChannel, value: i.value });
                     break;
                 case 'file':
-                    this.handlers.set('file', { execute: file, value: i.value });
+                    this.handlers.set('file', { execute: this.file, value: i.value });
                     break;
                 case 'socket':
-                    this.handlers.set('socket', { execute: socket, value: i.value });
+                    this.handlers.set('socket', { execute: this.socket, value: i.value });
                     break;
                 default:
                     this.handlers.set(i.type, { execute: i.handler, value: i.value })
@@ -51,10 +75,10 @@ export default class Logger {
         this.targets = config.targets.map(i => i.type)
     }
 
-    writeRaw(message) {
+    writeRaw(message, level) {
         this.targets.forEach(i => {
             const handler = this.handlers.get(i);
-            handler.execute(message, handler.value)
+            handler.execute(message, handler.value, this.levels.get(level))
         });
     }
 
@@ -65,18 +89,19 @@ export default class Logger {
                 message += string + _[i];
             });
             message = message.slice(0, -9);
-            this.writeRaw(`${this.showLoggingLevel ? 'sill' : ''} ${this.showTimestamps ? `[${(new Date()).toUTCString()}]` : ``} ${message}`)
+            this.writeRaw(`${this.showLoggingLevel ? 'sill' : ''} ${this.showTimestamps ? `[${(new Date()).toUTCString()}]` : ``} ${message}`, 4)
         }
     }
 
     info(strings, ..._) {
         if (this.maxLoggingLevel >= 3) {
+
             let message = '';
             strings.forEach((string, i) => {
                 message += string + _[i];
             });
             message = message.slice(0, -9);
-            this.writeRaw(`${this.showLoggingLevel ? 'info' : ''} ${this.showTimestamps ? `[${(new Date()).toUTCString()}]` : ``} ${message}`)
+            this.writeRaw(`${this.showLoggingLevel ? 'info' : ''} ${this.showTimestamps ? `[${(new Date()).toUTCString()}]` : ``} ${message}`, 3)
         }
     }
 
@@ -87,7 +112,7 @@ export default class Logger {
                 message += string + _[i];
             });
             message = message.slice(0, -9);
-            this.writeRaw(`${this.showLoggingLevel ? 'warn' : ''} ${this.showTimestamps ? `[${(new Date()).toUTCString()}]` : ``} ${message}`)
+            this.writeRaw(`${this.showLoggingLevel ? 'warn' : ''} ${this.showTimestamps ? `[${(new Date()).toUTCString()}]` : ``} ${message}`, 2)
         }
     }
 
@@ -97,19 +122,27 @@ export default class Logger {
             message += string + _[i];
         });
         message = message.slice(0, -9);
-        this.writeRaw(`${this.showLoggingLevel ? 'error' : ''} ${this.showTimestamps ? `[${(new Date()).toUTCString()}]` : ``} ${message}`)
-        if (this.exitOnError) process.exit(-1);
+        this.writeRaw(`${this.showLoggingLevel ? 'error' : ''} ${this.showTimestamps ? `[${(new Date()).toUTCString()}]` : ``} ${message}`, 1)
     }
-}
 
-export function discordChannel(message, channel) {
-    channel.send(message);
-}
+    discordChannel(message, channel, level) {
 
-export function file(message, path) {
-    appendFileSync(path, message, { encoding: 'utf-8' })
-}
 
-export function socket(message, socket) {
-    socket.write(message);
+        const embed = new MessageEmbed()
+            .setAuthor("gazza")
+            .setImage('https://i.ytimg.com/vi/M5JFeTtSS3c/maxresdefault.jpg')
+            .setThumbnail('https://cdn.shopify.com/s/files/1/0052/6666/9639/files/be_gay_do_crime_io_ascarium_large.jpg?v=1580670163')
+            .setDescription("smth happened")
+            .addField(`ㅤ`, message, true)
+            .setColor(level.color);
+        channel.send({ embeds: [embed] });
+    }
+
+    file(message, path, level) {
+        appendFileSync(path, message, { encoding: 'utf-8' })
+    }
+
+    socket(message, socket, level) {
+        socket.write(message);
+    }
 }
