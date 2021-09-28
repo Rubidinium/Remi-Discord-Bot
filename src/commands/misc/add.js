3e1d2b8f23571d79444c6ef1b2aeb4cc266ebf59
= <wolfencrime@gmail.com>
2021-09-28 21:09:50 +1000

    i did some things

--- src/commands/misc/add.js
+++ src/commands/misc/add.js
@@ -1,22 +1,27 @@
 const Commando = require("discord.js-commando");
 
 module.exports = class AddCommand extends Commando.Command {
-  constructor(client) {
-    super(client, {
-      name: "add",
-      aliases: ["sum"],
-      group: "misc",
-      memberName: "add",
-      description: "Adds multiple numbers together",
-      argsType: "multiple",
-    });
-  }
+	constructor(client) {
+		super(client, {
+			name: "add",
+			aliases: ["sum"],
+			group: "misc",
+			memberName: "add",
+			description: "Adds multiple numbers together",
+			argsType: "multiple",
+		});
+	}
 
-  async run(message, args) {
-    let sum = 0;
-    args.forEach((num) => {
-      sum += parseInt(num);
-    });
-    message.say(`The sum of ${args.join(" + ")} = ${sum}`);
-  }
+	async run(message, args) {
+		let sum = 0;
+		args.forEach((num) => {
+			const parsed = parseFloat(num);
+			if(isNaN(parsed)) {
+				message.say("you gotta give me an actual number to add lol");
+				return;
+			}
+			sum += parsed;
+		});
+		message.say(`The sum of ${args.join(" + ")} = ${sum}`);
+	}
 };
