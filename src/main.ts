84d60e9aa9df5e0afa4cdee83cbd5e1dbd376a0a
mx-mw <87119201+mx-mw@users.noreply.github.com>
2022-01-24 16:55:57 +1100

    eslint

--- src/main.ts
+++ src/main.ts
@@ -1,23 +1,23 @@
-import { Client, Intents, } from 'discord.js'
-import { config } from 'dotenv';
+import { Client, Intents, } from "discord.js";
+import { config } from "dotenv";
 config();
 
 class Bot extends Client {
 	constructor() {
 		super({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MEMBERS]});
-		this.once('ready', () => {
-			console.log('ready');
+		this.once("ready", () => {
+			console.log("ready");
 			this.user?.setActivity({
 				name: "hazim is gay",
 				type: "COMPETING"
 			});
 
-		})
+		});
 		this.login(process.env.TOKEN || "urmom");
 	}
 }
 
 (() => {
-	let bot = new Bot();
+	const bot = new Bot();
 	
-})()
+})();
