100ed972581e1bf299dd1de4a617ae02bfa7bfb9
Hazim Arafa <hazimarafa80@gmail.com>
2022-01-24 02:23:55 -0800

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -23,7 +23,6 @@ config();
 class Bot extends Client {
 	constructor() {
 		super({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_MESSAGE_REACTIONS, Intents.FLAGS.DIRECT_MESSAGES, Intents.FLAGS.DIRECT_MESSAGE_REACTIONS] });
-
 	}
 }
 
