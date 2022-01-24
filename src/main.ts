2706176eb0940683983cbcebdda7185395b94304
mx-mw <87119201+mx-mw@users.noreply.github.com>
2022-01-24 16:54:17 +1100

    hazim is gay

---
+++ src/main.ts
@@ -0,0 +1,23 @@
+import { Client, Intents, } from 'discord.js'
+import { config } from 'dotenv';
+config();
+
+class Bot extends Client {
+	constructor() {
+		super({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MEMBERS]});
+		this.once('ready', () => {
+			console.log('ready');
+			this.user?.setActivity({
+				name: "hazim is gay",
+				type: "COMPETING"
+			});
+
+		})
+		this.login(process.env.TOKEN || "urmom");
+	}
+}
+
+(() => {
+	let bot = new Bot();
+	
+})()
