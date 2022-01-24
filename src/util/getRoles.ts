f55cfa7f2c9f3a13d26005969c52c2c621275b69
Hazim Arafa <hazimarafa80@gmail.com>
2022-01-23 21:26:06 -0800

    init

--- src/util/getRoles.ts
+++
@@ -1,9 +0,0 @@
-import { Client, Collection, Role } from "discord.js";
-
-export default async function getRoles(client: Client): Promise<Collection<string, Role>> {
-	const all = await(await client.guilds.fetch("877584374521008199")).roles.fetch();
-	const bottomDiv = all.get("884289615719186442");
-	const topDiv = all.get("884289534366470197");
-	const courseRoles = all.filter((r: Role) => r.position > (bottomDiv?.position ?? 0) && r.position < (topDiv?.position ?? 69));
-	return courseRoles;
-}
--- src/util/purgeCache.ts
+++
@@ -1,5 +0,0 @@
-import { Collection } from "discord.js";
-import { _RATE_LIMIT_TIME } from "../server";
-export default function purgeCache(cache: Collection<string, number>): Collection<string, number> {
-	return cache.filter(i => new Date().getTime() - i < _RATE_LIMIT_TIME);
-}
