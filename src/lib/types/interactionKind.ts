e78f3b38650b411c0fd6e659157c58a7bd437825
mx-mw <87119201+mx-mw@users.noreply.github.com>
2022-01-25 16:06:24 +1100

    i forgor:skull:

--- src/lib/types/interactionKind.ts
+++ src/lib/types/interactionKind.ts
@@ -1,8 +1,8 @@
 import { CommandInteraction, ButtonInteraction, SelectMenuInteraction, ContextMenuInteraction, MessageComponentInteraction } from "discord.js";
 
-export type InteractionKind = 
-		| CommandInteraction 
-		| ButtonInteraction 
-		| SelectMenuInteraction 
-		| ContextMenuInteraction 
-		| MessageComponentInteraction;
+export type InteractionKind =
+	| CommandInteraction
+	| ButtonInteraction
+	| SelectMenuInteraction
+	| ContextMenuInteraction
+	| MessageComponentInteraction;
