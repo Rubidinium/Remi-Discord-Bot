aa0d5df0dc28336a3c2b09482b99ad6cdf08d68d
Hazim Arafa <hazimarafa80@gmail.com>
2022-01-24 22:17:29 -0800

    template

--- src/interactions/buttons/ticketArchive.ts
+++ src/interactions/buttons/ticketArchive.ts
@@ -5,4 +5,6 @@ const ARCHIVE_CATEGORY = "935403023159689226";
 export default async function (channel: GuildChannel) {
 	await channel.setParent(ARCHIVE_CATEGORY);
 	await channel.setName(`archive-${channel.name}`);
+
+
 }
